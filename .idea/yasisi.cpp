76ffe2662b959fb441b5a094eca911b39570f472
albert <abc@qwed.com>
2019-06-12 10:01:19 +0800

    yasisi01

---
+++ .idea/yasisi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by XXX on 2019/6/12.
+//
+
+#include "yasisi.h"
---
+++ .idea/yasisi.h
@@ -0,0 +1,16 @@
+//
+// Created by XXX on 2019/6/12.
+//
+
+#ifndef GITHUBTEST_YASISI_H
+#define GITHUBTEST_YASISI_H
+
+
+
+class yasisi {
+
+};
+
+
+
+#endif //GITHUBTEST_YASISI_H
